b8f386b2b52daebc97c636f43f6443124bb2e25f
przemskydev <przemekgithub@o2.pl>
2020-11-19 15:40:17 +0100

    create yargs commands

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 const chalk = require('chalk');
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+// const command = process.argv[2]
 
-if (command === 'add') {
-  console.log(chalk.green.inverse("Adding notes!"))
-} else if (command === 'remove') {
-  console.log(chalk.red.inverse("Removing notes!"))
-}
+// if (command === 'add') {
+//   console.log(chalk.green.inverse("Adding notes!"))
+// } else if (command === 'remove') {
+//   console.log(chalk.red.inverse("Removing notes!"))
+// }
 
-console.log(process.argv)
+// console.log(process.argv)
+// console.log(yargs.argv)
+
+//Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: ()=>console.log(chalk.green.inverse("Adding notes!"))
+})
+
+//Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: ()=>console.log(chalk.red.inverse("Removinging notes!"))
+})
+
+//Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List of notes',
+  handler: ()=>console.log(chalk.blue.inverse("Showing list of notes"))
+})
+
+//Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: ()=>console.log(chalk.yellow.inverse("Reading a single note"))
+})
+
+console.log(yargs.argv)
